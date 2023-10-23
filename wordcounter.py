6e847868ce4acc6a014fe880b7442f86fc998ba2
AwesomedudeX <102268445+AwesomedudeX@users.noreply.github.com>
2023-10-23 14:43:56 -0600

    Update wordcounter.py

--- wordcounter.py
+++ wordcounter.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
+st.title("Word Counter")
+
 uin = st.text_input("Enter text here:")
 uout = 0
 
@@ -8,4 +10,4 @@ for i in range(len(uin.split(" "))):
 		uout += 1
 
 if uin != "":
-	st.write(f"Length is {uout} words.")
+	st.write(f"This message is {uout} words long.")
