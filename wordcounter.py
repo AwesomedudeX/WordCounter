c0cf56cd4192f043bcccb1ad465137ff472bf6fe
AwesomedudeX <102268445+AwesomedudeX@users.noreply.github.com>
2023-10-24 12:59:31 -0600

    Update wordcounter.py

--- wordcounter.py
+++ wordcounter.py
@@ -2,13 +2,15 @@ import streamlit as st
 
 st.title("Word Counter")
 
-uin = st.text_input("Enter text here:")
+uin = st.text_input("Enter text here:").split(" ")
 uout = 0
 
-for i in range(len(uin.split(" "))):
-	if uin.split(" ")[i] not in [":", ";", ".", ",", "-", "+", "*"]:
-		uout += 1
-
+for i in range(len(uin)):
+	if uin[i] not in [":", ";", ".", ",", "-", "+", "*"]:
+		ulst = list(uin[i])
+		ulst.pop(" ")
+		uin[i] = ulst
+		
 if uin != "":
 	if uout == 1:
 		st.write(f"This message is {uout} word long.")
